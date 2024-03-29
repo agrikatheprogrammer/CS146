a0af90e05598e9aabee92342ffe107cd7186ae91
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-03-28 22:00:59 -0700

    Update Solution.java

--- HW/HW10/Solution.java
+++ HW/HW10/Solution.java
@@ -1,31 +1,28 @@
 public class Solution {
-            public static List<List<Integer>> levelOrder(TreeNode root) {
-            if (root==null)
+public static List<List<Integer>> levelOrder(TreeNode root) {
+            if (root == null)
                 return null;
-            List<List<Integer>> result= new ArrayList<>();
-            Queue<TreeNode> qe=new LinkedList<>();
+            List<List<Integer>> result = new ArrayList<>();
+            Queue<TreeNode> qe = new LinkedList<>();
             qe.add(root);
-            int count=1;
-            List<Integer> toadd=new ArrayList<>();
-            while (count!=0) {
-                if (qe.peek()!=null) {
-                    toadd.add(qe.peek().val);
-                    if (qe.peek().right != null)
-                        qe.add(qe.peek().right);
-                    if (qe.peek().left != null)
-                        qe.add(qe.peek().left);
-                    qe.remove();
-                    count--;
-                }
-                if(count == 0 )
-                {
-                    result.add(toadd);
-                    toadd=new ArrayList<>();
-                    count = qe.size();
-                }
+            int count = 1;
+            List<Integer> toadd = new ArrayList<>();
+            while (!qe.isEmpty()) {
+                toadd.add(qe.peek().val);
+                if (qe.peek().right != null)
+                    qe.add(qe.peek().right);
+                if (qe.peek().left != null)
+                    qe.add(qe.peek().left);
+                qe.remove();
+                count--;
+            if (count == 0) {
+                result.add(toadd);
+                toadd = new ArrayList<>();
+                count = qe.size();
+            }
             }
             return result;
-            }
+        }
 }
 
 
