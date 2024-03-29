e254d30dad7e345fbb6e473f8cb227715366bcd4
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-03-28 21:20:53 -0700

    Update solution.py

--- HW/HW10/solution.py
+++ HW/HW10/solution.py
@@ -1,1 +1,13 @@
+class solution:
+  def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
 
+class TreeNode(object):
+
+     def __init__(self, val=0, left=None, right=None):
+
+         self.val = val
+
+         self.left = left
+
+         self.right = right
+  
