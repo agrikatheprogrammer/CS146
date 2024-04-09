f249c6c79fc7911691368cfc1eb4fc64a0d9daee
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-09 13:34:26 -0700

    Update Solution.java

--- HW/HW11/Solution.java
+++ HW/HW11/Solution.java
@@ -1,1 +1,18 @@
+ public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+    if (
+        sr < 0 ||
+        sc < 0 ||
+        sr >= (int)image.size() ||
+        sc >= (int)image[r].size() ||
+        image[sr][sc] == newColor ||
+        image[sr][sc] != oldColor
+    ) 
+        return;
+    
 
+    image[sr][sc] = newColor;
+    floodFill(image, sr - 1, sc, newColor, oldColor);
+    floodFill(image, sr, sc - 1, newColor, oldColor);
+    floodFill(image, sr, sc + 1, newColor, oldColor);
+    floodFill(image, sr + 1, sc, newColor, oldColor);
+}
