3b6407f9a2b24f052c3f11728303aaba5c5328db
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-09 13:56:49 -0700

    Update Solution.java

--- EC/EverythingEverywhereAllAtOnce/Solution.java
+++ EC/EverythingEverywhereAllAtOnce/Solution.java
@@ -1,1 +1,17 @@
+import java.util.*;
+
+public class Graph {
+    public int[][] adjacencyListToMatrix(List<List<Integer>> adjacencyList) {
+        int n = adjacencyList.size();
+        int[][] matrix = new int[n][n];
+        
+        for (int v = 0; v < n; v++) {
+            for (int u : adjacencyList.get(v)) {
+                matrix[v][u] = 1;
+            }
+        }
+        
+        return matrix;
+    }
+}
 
