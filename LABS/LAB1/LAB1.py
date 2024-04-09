422d6f74668dcdb3b953697fe5cdd667b9849007
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-09 14:19:40 -0700

    Update LAB1.py

--- LABS/LAB1/LAB1.py
+++ LABS/LAB1/LAB1.py
@@ -19,5 +19,13 @@ class Solution:
         raise ValueError("No two sum solution")
 
     # Two pass
-    def twoSum(self, nums: 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        for i, num in enumerate(nums):
+            num_map[num] = i
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map and num_map[complement] != i:
+                return [i, num_map[complement]]
+        raise ValueError("No two sum solution")
 
