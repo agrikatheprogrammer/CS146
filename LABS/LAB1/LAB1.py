3ea9898a434414559ec362b398efa64f277cbed2
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-09 14:18:49 -0700

    Update LAB1.py

--- LABS/LAB1/LAB1.py
+++ LABS/LAB1/LAB1.py
@@ -1,1 +1,23 @@
+class Solution:
+    # One pass hash table
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        raise ValueError("No two sum solution")
+
+    # Brute force
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        raise ValueError("No two sum solution")
+
+    # Two pass
+    def twoSum(self, nums: 
 
