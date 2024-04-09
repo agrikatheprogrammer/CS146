43e9b8fd9e2f5bc4a799360aaa2a8912cd79e92f
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-09 13:58:42 -0700

    Update Solution.java

--- EC/EverythingEverywhereAllAtOnce/Solution.java
+++ EC/EverythingEverywhereAllAtOnce/Solution.java
@@ -13,5 +13,39 @@ public class Graph {
         
         return matrix;
     }
+
+    public List<List<Integer>> adjacencyMatrixToList(int[][] matrix) {
+        int n = matrix.length;
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            List<Integer> neighbors = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    neighbors.add(j);
+                }
+            }
+            adjacencyList.add(neighbors);
+        }
+        
+        return adjacencyList;
+    }
+
+    public List<List<Integer>> reverseDirectedGraph(List<List<Integer>> graph) {
+        int n = graph.size();
+        List<List<Integer>> reversedGraph = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            reversedGraph.add(new ArrayList<>());
+        }
+        
+        for (int v = 0; v < n; v++) {
+            for (int u : graph.get(v)) {
+                reversedGraph.get(u).add(v);
+            }
+        }
+        
+        return reversedGraph;
+    }
 }
 
