f0d3acfe60f8886ebc9589c35f951123048e60d3
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-12 01:41:43 -0700

    Update Solution.java

--- LABS/LAB2/Solution.java
+++ LABS/LAB2/Solution.java
@@ -1,4 +1,6 @@
 class Solution{
+
+  //O(n)
   public static boolean isAnagram(String s,String v){
     ArrayList<Character> s_chars=charsInString(s);
     ArrayList<Character> v_chars=charsInString(v);
@@ -24,6 +26,19 @@ class Solution{
     return result;
   }
 
+// O(n^2)
+boolean isAnagramSort(String string1, String string2) {
+    if (string1.length() != string2.length()) {
+        return false;
+    }
+    char[] a1 = string1.toCharArray();
+    char[] a2 = string2.toCharArray();
+    Arrays.sort(a1);
+    Arrays.sort(a2);
+    return Arrays.equals(a1, a2);
+}
+
+  //O(n)
   public boolean isAnagramCounting(String string1, String string2) {
     if (string1.length() != string2.length()) {
         return false;
