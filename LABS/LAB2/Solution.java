036e1368c82c0b71071c29841257fe5db9e72e29
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-12 01:19:41 -0700

    Create Solution.java

---
+++ LABS/LAB2/Solution.java
@@ -0,0 +1,31 @@
+class Solution{
+  public static boolean isAnagram(String s,String v){
+    ArrayList<Character> s_chars=charsInString(s);
+    ArrayList<Character> v_chars=charsInString(v);
+    boolean sSubv=true;
+    boolean vSubs=true;
+    for (Character a:s_chars){
+      if (!v_chars.contains(a))
+        sSubv=false;
+    }
+    for (Character a:v_chars){
+      if (!s_chars.contains(a))
+        vSubs=false;
+    }
+    return vSubs&&sSubv;
+  }
+  
+
+  public static ArrayList<Character> charsInString(String s){
+    ArrayList<Character> result;
+    for (int i=0;i<s.size().i++){ 
+      result.add(s.charAt(i));
+    }
+  }
+
+  public static void main(){
+    String a="mwahaha";
+    String b="ahahmaw";
+    System.out.println(isAnagram(a,b));
+  }
+}
