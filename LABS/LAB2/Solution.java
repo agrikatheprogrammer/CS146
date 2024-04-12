0b29164c81fba43076264854e8725c1c7616301c
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-12 01:45:14 -0700

    Update Solution.java

--- LABS/LAB2/Solution.java
+++ LABS/LAB2/Solution.java
@@ -26,7 +26,7 @@ class Solution{
     return result;
   }
 
-// O(n^2)
+// O(nlogn)
 boolean isAnagramSort(String string1, String string2) {
     if (string1.length() != string2.length()) {
         return false;
