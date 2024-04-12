ccdaa4acd494986e5edb60e7a6333e4cbb6fff42
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-12 01:32:14 -0700

    Update Solution.java

--- LABS/LAB2/Solution.java
+++ LABS/LAB2/Solution.java
@@ -24,6 +24,23 @@ class Solution{
     return result;
   }
 
+  public boolean isAnagramCounting(String string1, String string2) {
+    if (string1.length() != string2.length()) {
+        return false;
+    }
+    int count[] = new int[CHARACTER_RANGE];
+    for (int i = 0; i < string1.length(); i++) {
+        count[string1.charAt(i)]++;
+        count[string2.charAt(i)]--;
+    }
+    for (int i = 0; i < CHARACTER_RANGE; i++) {
+        if (count[i] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
   public static void main(String[] args){
     String a="mwahaha";
     String b="ahahmaw";
