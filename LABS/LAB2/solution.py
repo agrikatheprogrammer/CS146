756fa99cc8033bcb5f0710577a2d736deee02e44
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-12 10:28:23 -0700

    Update solution.py

--- LABS/LAB2/solution.py
+++ LABS/LAB2/solution.py
@@ -2,15 +2,15 @@ class Solution:
   
     @staticmethod
     def is_anagram(s, v):
-        s_chars = s.split()
-        v_chars = v.split()
+        s_chars = set(s)
+        v_chars = set(v)
         s_sub_v = all(a in v_chars for a in s_chars)
         v_sub_s = all(a in s_chars for a in v_chars)
         return s_sub_v and v_sub_s
 
     @staticmethod
-    def is_anagram(s, v):
-        return len(string1) == len(string2) and sorted(s) == sorted(v)
+    def is_anagram_sort(s, v):
+        return len(s) == len(v) and sorted(s) == sorted(v)
  
     @staticmethod
     def is_anagram_counting(string1, string2):
@@ -31,5 +31,6 @@ if __name__ == "__main__":
     print()
     print(Solution.is_anagram(b, d))
     print(Solution.is_anagram_sort(b, d))
-    print(Solution.is_anagram_counting(b, d)))
+    print(Solution.is_anagram_counting(b, d))
+
 
