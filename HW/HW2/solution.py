ce753117825424bb64ac498eeb1434fadc335a8c
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-19 14:15:16 -0700

    Update solution.py

--- HW/HW2/solution.py
+++ HW/HW2/solution.py
@@ -3,4 +3,15 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
+        low = 1
+        high = n
+        while low <= high:
+        mid = low + (high - low) // 2
+        if not isBadVersion(mid):
+            low = mid + 1
+        elif isBadVersion(mid - 1):
+            return mid
+        else:
+            high=mid-1
+        return -1
     
