8109e77fc0ee4a43d33c82789fc67c4895e59c69
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-19 12:03:54 -0700

    Update Solution.java

--- HW/HW1/Solution.java
+++ HW/HW1/Solution.java
@@ -1,10 +1,17 @@
-public class Solution {
-  public boolean isPalindrome(String s) {
+class Solution {
+    public boolean isPalindrome(String s) {
     boolean isIt=true;
-    int n=s.length();
+    String temp="";
+    for (int i=0;i<s.length();i++) {
+        if (Character.isLetterOrDigit(s.charAt(i))){
+            temp+=s.charAt(i);
+        }
+    }
+    temp=temp.toLowerCase();
+    int n=temp.length();
     //compare char by char first char ==last? second char==second last?..
     for (int i=0;i<n;i++) {
-      if (s.charAt(i)!=s.charAt(n-i-1)) {
+      if (temp.charAt(i)!=temp.charAt(n-i-1)) {
         isIt=false;
         break;
       }
