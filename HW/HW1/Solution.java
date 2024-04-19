926b47c173ae52f604ac2c533172d620af0916cb
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-19 13:43:24 -0700

    Update Solution.java

--- HW/HW1/Solution.java
+++ HW/HW1/Solution.java
@@ -16,6 +16,32 @@ class Solution {
   }
 
 
+    //4th solution(3rd and 2nd below)
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+        	char currFirst = s.charAt(start);
+        	char currLast = s.charAt(last);
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		start++;
+        	} else if(!Character.isLetterOrDigit(currLast)) {
+        		last--;
+        	} else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
+        }
+        return true;
+    }
+    
+
 
     // passes 479/485 cases: memory exceeds for long strings
     public boolean isPalindrome(String s) {
