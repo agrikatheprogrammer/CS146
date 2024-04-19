84e39f6ea7421702b19d02d902d3d19fd9a581da
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-19 14:49:13 -0700

    Create Solution.java

---
+++ EC/ThirdLightbulb/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+public static int fibonacci(int n) {
+        if (n <= 1)
+            return n;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; ++i) {
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        return b;
+    }
+}
