87fab3b6bb7aaa3d9eb4e769d9b0987c0746917c
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-19 13:57:11 -0700

    Update Solution.java

--- HW/HW2/Solution.java
+++ HW/HW2/Solution.java
@@ -1,6 +1,9 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        
+        for (int i=0;i<n;i++) {
+            if (isBadVersion(i))
+                return i;
+        }
     }
 }
