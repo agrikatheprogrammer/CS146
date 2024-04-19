4ad6074d598e6ece6f6608571799a20749e496c3
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-19 14:10:22 -0700

    Update Solution.java

--- HW/HW2/Solution.java
+++ HW/HW2/Solution.java
@@ -3,7 +3,6 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int low = 0;
         int high = n;
-
         while(low<=high){
             int mid = low + (high-low)/2;
             if(isBadVersion(mid)==false ) low = mid+1;
