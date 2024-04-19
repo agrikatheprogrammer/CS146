b491f1866067de92831ade6327c81d7d45a42e8f
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-19 11:54:13 -0700

    Update Solution.java

--- HW/HW1/Solution.java
+++ HW/HW1/Solution.java
@@ -1,1 +1,14 @@
-
+public class Solution {
+  public boolean isPalindrome(String s) {
+    boolean isIt=true;
+    int n=s.length();
+    //compare char by char first char ==last? second char==second last?..
+    for (int i=0;i<n;i++) {
+      if (s.charAt(i)!=s.charAt(n-i-1)) {
+        isIt=false;
+        break;
+      }
+    }  
+    return isIt;
+  }
+}
