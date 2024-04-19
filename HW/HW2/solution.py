a7570d285002c907f670c7184cafb43add08819b
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-19 14:20:50 -0700

    Update solution.py

--- HW/HW2/solution.py
+++ HW/HW2/solution.py
@@ -9,7 +9,7 @@ class Solution:
             mid = low + (high - low) // 2
             if not isBadVersion(mid):
                 low = mid + 1
-            elif isBadVersion(mid - 1):
+            elif not isBadVersion(mid - 1):
                 return mid
             else:
                 high=mid-1
