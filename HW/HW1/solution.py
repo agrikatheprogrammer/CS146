83ce8543bbd57ffd5e888afb673ffdf5505c3481
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-19 12:04:48 -0700

    Update solution.py

--- HW/HW1/solution.py
+++ HW/HW1/solution.py
@@ -1,1 +1,20 @@
+class solution:
+    def isPalindrome(self, s: str) -> bool:
+        isIt = True
+        temp = ""
+        
+        for char in s:
+            if char.isalnum():
+                temp += char
+                
+        temp = temp.lower()
+        n = len(temp)
+        
+        # Compare char by char: first char == last? second char == second last?..
+        for i in range(n):
+            if temp[i] != temp[n - i - 1]:
+                isIt = False
+                break
+                
+        return isIt
 
