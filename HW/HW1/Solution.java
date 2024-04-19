f08dcd84015262db572a1e4dd2bae6e87072d5fa
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-19 13:19:35 -0700

    Update Solution.java

--- HW/HW1/Solution.java
+++ HW/HW1/Solution.java
@@ -2,7 +2,6 @@ class Solution {
     //1st sol. 
     //beats only 4% w java but this code in python beats 67% of submissions
     public boolean isPalindrome(String s) {
-    boolean isIt=true;
     String temp="";
     for (int i=0;i<s.length();i++) {
         if (Character.isLetterOrDigit(s.charAt(i))){
@@ -16,11 +15,10 @@ class Solution {
     //compare char by char first char ==last? second char==second last?..
     for (int i=0;i<n;i++) {
       if (temp.charAt(i)!=temp.charAt(n-i-1)) {
-        isIt=false;
-        break;
+        return false;
       }
     }  
-    return isIt;
+        return true;
   }
 
 
